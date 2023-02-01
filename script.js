cb65640026dbb4e6b018297f9c1c01cf2a87c087
Jose Serrano <serranojose.ar@gmail.com>
2023-02-01 15:54:54 -0300

    Primer comimit

---
+++ script.js
@@ -0,0 +1,40 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector(' .screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else{
+        handleSymbol(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if(previousOperator === null){
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case '◀':
+            if (buffer.length ===1){
+                buffer = '0';
+            }else{
+                buffer = toString(0, buffer.length - 1);
+            }
+            break;
+        case
+    }
+}
